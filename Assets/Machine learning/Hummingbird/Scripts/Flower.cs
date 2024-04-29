a5e4fb127f66bd2748a400d53bc16b460e76b83c
Skyron <courtier.leo@gmail.com>
2024-04-29 11:44:33 +0200

    Reprise machine learning

--- Assets/Machine learning/Hummingbird/Scripts/Flower.cs
+++ Assets/Machine learning/Hummingbird/Scripts/Flower.cs
@@ -1,72 +1,86 @@
 using System;
 using UnityEngine;
 
-
-namespace Hummingbird {
+namespace MachineLearning
+{
     /// <summary>
     /// Manages a single flower with nectar
     /// </summary>
-    public class Flower : MonoBehaviour {
-
-        [Tooltip("The color when the flower is full"), SerializeField] private Color fullFlowerColor = new Color(1f, 0f, .3f);
-        [Tooltip("The color when the flower is full"), SerializeField] private Color emptyFlowerColor = new Color(.5f, 0f, 1f);
+    public class Flower : MonoBehaviour
+    {
+        [Tooltip("The color when the flower is full")]
+        public Color fullFlowerColor = new Color(1f,0f,.3f);
+        
+        [Tooltip("The color when the flower is empty")]
+        public Color emptyFlowerColor = new Color(.4f,0f,1f);
 
         /// <summary>
         /// The trigger collider representing the nectar
         /// </summary>
-        [HideInInspector] public Collider nectarCollider;
-
-        /// <summary>
-        /// The solid collider representing the flower petals
-        /// </summary>
+        [HideInInspector]
+        public Collider nectarCollider;
+        
+        // The solid collider representing the flower petals
         private Collider _flowerCollider;
-
+        
+        // The flower's material
         private Material _flowerMaterial;
-
+        
+        /// <summary>
+        /// A vector pointing straight out of the flower
+        /// </summary>
         public Vector3 FlowerUpVector => nectarCollider.transform.up;
 
+        /// <summary>
+        /// The center position of the nectar collider
+        /// </summary>
         public Vector3 FlowerCenterPosition => nectarCollider.transform.position;
-        
+        /// <summary>
+        /// The amount of nectar remaining in the flower
+        /// </summary>
         public float NectarAmount { get; private set; }
 
+        /// <summary>
+        /// Whether the flower has any nectar remaining
+        /// </summary>
         public bool HasNectar => NectarAmount > 0f;
-
-        private void Awake() {
-            _flowerMaterial = GetComponent<MeshRenderer>().material;
-            _flowerCollider = GetComponent<Collider>();
-            nectarCollider = transform.Find("Pistil").GetComponent<Collider>();
-        }
 
         /// <summary>
         /// Attempts to remove nectar from the flower
         /// </summary>
         /// <param name="amount">The amount of nectar to remove</param>
         /// <returns>The actual amount successfully removed</returns>
-        public float Feed(float amount) {
+        public float Feed(float amount)
+        {
             float nectarTaken = Mathf.Clamp(amount, 0f, NectarAmount);
+            
             NectarAmount -= amount;
-            if (HasNectar) {
-                NectarAmount = 0f;
-                
+
+            if (NectarAmount <= 0) {
+                NectarAmount = 0;
                 _flowerCollider.gameObject.SetActive(false);
                 nectarCollider.gameObject.SetActive(false);
-
                 _flowerMaterial.SetColor("_BaseColor", emptyFlowerColor);
             }
 
             return nectarTaken;
         }
-
-        /// <summary>
-        /// Resets the flower
-        /// </summary>
-        public void ResetFlower() {
+        
+        public void ResetFlower()
+        {
             NectarAmount = 1f;
-            
             _flowerCollider.gameObject.SetActive(true);
             nectarCollider.gameObject.SetActive(true);
-            
             _flowerMaterial.SetColor("_BaseColor", fullFlowerColor);
+        }
+
+        private void Awake()
+        {
+            MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+            _flowerMaterial = meshRenderer.material;
+            _flowerCollider = transform.Find("FlowerCollider").GetComponent<Collider>(); 
+            nectarCollider = transform.Find("FlowerNectarCollider").GetComponent<Collider>();
+            ResetFlower();
         }
     }
 }
--- Assets/Machine learning/Hummingbird/Scripts/FlowerArea.cs
+++ Assets/Machine learning/Hummingbird/Scripts/FlowerArea.cs
@@ -1,96 +1,78 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
-using Unity.Burst;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-namespace Hummingbird {
+namespace MachineLearning
+{
     /// <summary>
     /// Manages a collection of flower plants and attached flowers
     /// </summary>
-    public class FlowerArea : MonoBehaviour {
-        
-        //The diameter of the area where the agent and flowers can be used for observing relative distance from agent to flower
+    public class FlowerArea : MonoBehaviour
+    {
         public const float AreaDiameter = 20f;
-        
-        //The list of all the flower plants in the flower area
+
         private List<GameObject> _flowerPlants;
-        
-        //A lookup dictionary for looking up a flower from a nectar collider
+
         private Dictionary<Collider, Flower> _nectarFlowerDictionary;
-
-        /// <summary>
-        /// The list of all flowers in the flower area
-        /// </summary>
-        public List<Flower> Flowers { get; private set; }
         
-        private void OnDrawGizmos() {
-            Gizmos.color = Color.cyan;
-            Gizmos.DrawWireSphere(transform.position, AreaDiameter);
-        }
+        public List<Flower> Flowers { get; private set; }
 
-        /// <summary>
-        /// Reset flowers and flower plants
-        /// </summary>
-        public void ResetFlowers() {
-            //Rotate each flower plant around the Y axis and 
-            foreach (GameObject flowerPlant in _flowerPlants) {
-                float xRotation = UnityEngine.Random.Range(-5f, 5f);
-                float yRotation = UnityEngine.Random.Range(-180f, 180f);
-                float zRotation = UnityEngine.Random.Range(-5f, 5f);
+        public void ResetFlowers()
+        {
+            // Rotate each flower plant around the Y axis and subtly around X and Z
+            foreach (var flowerPlant in _flowerPlants)
+            {
+                float xRotation = Random.Range(-5f, 5f);
+                float yRotation = Random.Range(-180f, 180f);
+                float zRotation = Random.Range(-5f, 5f);
                 flowerPlant.transform.localRotation = Quaternion.Euler(xRotation, yRotation, zRotation);
             }
-            
-            //Reset each flower
-            foreach (Flower flower in Flowers) {
+
+            foreach (var flower in Flowers) {
                 flower.ResetFlower();
             }
         }
 
         /// <summary>
-        /// Gets the <see cref="Flower"/> that a nectar collider belong to
+        /// Get the <see cref="Flower"/> that a nectar collier belongs to
         /// </summary>
         /// <param name="collider">The nectar collider</param>
         /// <returns>The matching flower</returns>
-        public Flower GetFlowerFromNectar(Collider collider) {
+        public Flower GetFlowerFromNectar(Collider collider)
+        {
             return _nectarFlowerDictionary[collider];
         }
 
-        private void Awake() {
-            //Initialize variables
+        private void Awake()
+        {
             _flowerPlants = new List<GameObject>();
             _nectarFlowerDictionary = new Dictionary<Collider, Flower>();
             Flowers = new List<Flower>();
         }
 
-        private void Start() {
-            // Find all flowers that are children of this GameObject/Transform
-            FindChildFlowers(transform);
+        private void Start()
+        {
+            FindChildFlower(transform);
         }
-        
-        /// <summary>
-        /// Recursively find all flowers and flower plants that are children of a transform parent
-        /// </summary>
-        /// <param name="parent">The parennt of the children to check</param>
-        private void FindChildFlowers(Transform parent) {
-            for (int i = 0; i < parent.childCount; i++) {
+
+        private void FindChildFlower(Transform parent)
+        {
+            for (int i = 0; i < parent.childCount; i++)
+            {
                 Transform child = parent.GetChild(i);
                 if (child.CompareTag("flower_plant")) {
-                    //Found a flower plant, add it to the flowerPlants list
                     _flowerPlants.Add(child.gameObject);
-                    
-                    //Look for the flowers within the flower plant
-                    FindChildFlowers(child);
+                    FindChildFlower(child);
                 }
-                else {
-                    //Not a flower plant, look for a Flower component
-                    Flower flower = child.GetComponent<Flower>();
-                    if (flower != null) {
+                else
+                {
+                    if (child.TryGetComponent(out Flower flower)) {
                         Flowers.Add(flower);
-                        
                         _nectarFlowerDictionary.Add(flower.nectarCollider, flower);
                     }
                     else {
-                        FindChildFlowers(child);
+                        FindChildFlower(child);
                     }
                 }
             }
